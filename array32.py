d16d325aa4894a45864172534669e403ee347762
Star <111043921+PranavShilavane@users.noreply.github.com>
2022-08-27 23:15:25 +0530

    Add files via upload

---
+++ array32.py
@@ -0,0 +1,53 @@
+'''
+
+Multiply two strings
+MediumAccuracy: 26.16%Submissions: 100k+Points: 4
+Given two numbers as strings s1 and s2. Calculate their Product.
+Multiply two strings
+MediumAccuracy: 26.16%Submissions: 100k+Points: 4
+Given two numbers as strings s1 and s2. Calculate their Product.
+Note: The numbers can be negative.
+
+
+Example 1:
+
+Input:
+s1 = "33"
+s2 = "2"
+
+Output:
+66
+
+Example 2:
+
+Input:
+s1 = "11"
+s2 = "23"
+
+Output:
+253
+
+Note: The numbers can be negative.
+
+Example 1:
+
+Input:
+s1 = "33"
+s2 = "2"
+Output:
+66
+Example 2:
+
+Input:
+s1 = "11"
+s2 = "23"
+Output:
+253
+
+
+'''
+
+string1 = "-23"
+string2 = "-3"
+
+print(int(string1)*int(string2))
